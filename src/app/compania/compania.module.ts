e57c3c13ef346779d2a4384a48860081a0a7942c
SAMANTHITO <alfons17ver@gmail.com>
2024-03-12 00:57:25 -0500

    todas las rutas funcionan padre e hijas de compania

---
+++ src/app/compania/compania.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListadoComponent } from './listado/listado.component';
+import { AgregarComponent } from './agregar/agregar.component';
+import { EditarComponent } from './editar/editar.component';
+import { EliminarComponent } from './eliminar/eliminar.component';
+import { HomeComponent } from './home/home.component';
+import { CompaniaRoutingModule } from './compania-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    ListadoComponent,
+    AgregarComponent,
+    EditarComponent,
+    EliminarComponent,
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    CompaniaRoutingModule
+  ],
+  exports:[
+    CompaniaRoutingModule
+  ]
+})
+export class CompaniaModule { }
